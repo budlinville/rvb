0581573a9c94a6cb3e3cd1e9d6dd807f049814f4
budlinville <budlinville@gmail.com>
2023-05-30 22:01:01 -0500

    Added click sounds

--- src/components/pages/Home/Button.tsx
+++ src/components/pages/Home/Button.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from 'react';
+
 import classes from './home.module.css';
+import clickSound from '/click.mp3';
 
 export type ButtonT = 'red' | 'blue';
+
+
+const audio = new Audio(clickSound);
+
 
 interface ButtonProps {
     children: ReactNode;
@@ -10,8 +16,13 @@ interface ButtonProps {
 
 const Button = ({ children, type }: ButtonProps) => {
     const colorClassName = type === 'red' ? classes.red : classes.blue;
+
+    const onMouseDownHandler = () => {
+        audio.play();
+    };
+
     return (
-        <button className={classes.pushable}>
+        <button className={classes.pushable} onMouseDown={onMouseDownHandler}>
             <span className={classes.shadow} />
             <span className={`${classes.edge} ${colorClassName}`} />
             <span className={`${classes.front} ${colorClassName}`}>
