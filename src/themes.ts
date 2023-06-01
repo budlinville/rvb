1df14ad86cea6a2b7b887f9cef9da64ddd370e95
budlinville <budlinville@gmail.com>
2023-06-01 13:06:18 -0500

    Moved configured colors from home.module.css to index.css

--- src/themes.ts
+++ src/themes.ts
@@ -4,7 +4,7 @@ import { PaletteMode } from '@mui/material';
 import { grey } from "@mui/material/colors";
 
 
-// These are defined in home.module.css
+// These are defined in index.css
 const RED           = getComputedStyle(document.documentElement).getPropertyValue('--RED');
 const DARK_RED      = getComputedStyle(document.documentElement).getPropertyValue('--DARK-RED');
 const LIGHT_RED     = getComputedStyle(document.documentElement).getPropertyValue('--LIGHT-RED');
