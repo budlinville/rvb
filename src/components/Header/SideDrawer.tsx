22e465a0da7ad68b0ebcda968a2a70dea9718ae2
budlinville <budlinville@gmail.com>
2023-06-07 13:37:03 -0500

    Improved About page

--- src/components/Header/SideDrawer.tsx
+++ src/components/Header/SideDrawer.tsx
@@ -37,9 +37,10 @@ interface DividerT { id: number, divider: boolean };
 const menuItems: (DrawerPageT | DividerT)[] = [
     { id: 1, label: 'Home',     route: HOME_PATH,       icon: <CottageIcon /> },
     { id: 2, divider: true },
+    { id: 5, label: 'Stats',    route: STATS_PATH,      icon: <AssessmentIcon /> },
     { id: 3, label: 'About',    route: ABOUT_PATH,      icon: <ImportContactsIcon /> },
     { id: 4, label: 'Support',  route: SUPPORT_PATH,    icon: <AccessibilityNewIcon /> },
-    { id: 5, label: 'Stats',    route: STATS_PATH,      icon: <AssessmentIcon /> },
+    { id: 2, divider: true },
     { id: 6, label: 'Settings', route: SETTINGS_PATH,   icon: <SettingsIcon /> },
 ];
 
--- src/components/pages/About/index.tsx
+++ src/components/pages/About/index.tsx
@@ -1,16 +1,25 @@
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
+import { Link } from 'react-router-dom';
+import { SUPPORT_PATH } from '../Support';
 
 const About = () => {
 
     return (
         <Container>
             <Box sx={{ my: 2 }}>
-                <Typography variant="h3" component="div"> About </Typography>
-                <Divider />
-                    
+                <Typography variant='h3' component='div'> About </Typography>
+                <Divider sx={{ my: 2 }}/>
+                <Typography variant='h4'>
+                    This project started as all great projects start - after 12 too many beers in college, lying on my couch thinking about to make more money. I've always been attracted to building out applications that people can use to improve their lives. This application is not that. This application was merely a way for me to fine-tune my development skills, but may also serve as a fun social experiment.
+                </Typography>
+                <br />
+                <Typography variant='h4'>
+                    That being said, please visit my <Link to={SUPPORT_PATH}>Support</Link> page if you enjoyed this application and want to sponsor me as I build more productive applications!
+                </Typography>
+
             </Box>
         </Container>
     );
