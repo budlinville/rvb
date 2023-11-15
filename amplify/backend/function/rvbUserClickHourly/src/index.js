007f18b844919a09073fee103ed1e28805cb5879
budlinville <budlinville@gmail.com>
2023-11-14 19:19:57 -0600

    Fixed not running correctly locally

--- amplify/backend/function/rvbUserClickHourly/src/index.js
+++ amplify/backend/function/rvbUserClickHourly/src/index.js
@@ -14,7 +14,7 @@ const ENV_TABLE_MAP = {
     master: 'rvb-click-prod',
 };
 
-const TABLE_NAME = ENV_TABLE_MAP[process.env.ENV];
+const TABLE_NAME = ENV_TABLE_MAP[process.env.ENV || 'staging'];
 
 
 //----------------------------------------------------------------------------------------------------------------------
