84da8862d07330a8bbdd9757327ffb872900aaeb
budlinville <budlinville@gmail.com>
2023-11-17 10:56:54 -0600

    Removed unused useEffect import from Profile

--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useContext, useEffect } from "react";
+import { CSSProperties, useContext } from "react";
 
 import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
